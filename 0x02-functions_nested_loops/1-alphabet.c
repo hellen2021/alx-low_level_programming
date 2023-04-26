f961e322446161017a884056e24ea42c568bee72
hellen2021 <elleville23@gmail.com>
2023-04-26 00:10:01 -0700

    print alphabets

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,16 +1,15 @@
 #include "main.h"
+
 /**
- * print alphabets
- * return void
+ * print_alphabet - Make the alphabet
+ *
+ * Return:void
  */
-void print_alphabet(){
-char c;
-for (c = 'a'; c<='z'; c++)
-	_putchar("%c", c)
-}
-_putchar("\n")
-}
-int main(){
-print_alphabet();
-return 0;l
+
+void print_alphabet(void)
+{
+char  ch;
+for (ch = 'a'; ch <= 'z'; ch++)
+_putchar(ch);
+_putchar('\n');
 }
